c6d55e4197ea649c6cd064dfd03dbd400bad3132
aartyomm <artem-kostin2013.kostin@yandex.ru>
2026-03-28 21:51:51 +0300

    added lab5

---
+++ turtle_controller_pkg_2/turtle_controller_pkg/pose_listener.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+
+class PoseListener(Node):
+    def __init__(self):
+        super().__init__('pose_listener')
+        self.sub = self.create_subscription(Pose, '/turtle1/pose', self.on_pose, 10)
+
+    def on_pose(self, msg):
+        self.get_logger().info(
+            f'x={msg.x:.2f} y={msg.y:.2f} theta={msg.theta:.2f}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseListener()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
